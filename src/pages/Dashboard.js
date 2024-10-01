90588815854fff903ed94536174fca341428f1b0
amanuel <137877737+agdgb@users.noreply.github.com>
2024-10-01 15:24:36 +0300

    enhance axios interceptor handling of token refresh

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../services/api";
 
-function Dashboard() {
-  return <div>Dashboard conetnet</div>;
-}
+const Dashboard = () => {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const ff = async () => {
+      const res = await api.get("/api/users/user");
+      setResponse(res.data);
+    };
+
+    ff();
+  }, []);
+
+  const accessAdmin = async () => {
+    const res = await api.get("/api/users/admin", {
+      token: localStorage.getItem("refreshToken"),
+    });
+    setResponse(res.data);
+  };
+  const accessUser = async () => {
+    const res = await api.get("/api/users/user");
+    setResponse(res.data);
+  };
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        <p>Endpoint Accessed: {response.message}</p>
+        <button onClick={accessUser}>User</button> &nbsp;
+        <button onClick={accessAdmin}>Admin</button> &nbsp;
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,62 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    let originalRequest = error.config;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        if (!refreshToken) {
+          throw new Error("Refresh token is missing");
+        }
+
+        const res = await api.post("/api/users/refresh", {
+          token: refreshToken,
+        });
+
+        localStorage.setItem("token", res.data.accessToken);
+        localStorage.setItem("refreshToken", res.data.refreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
+
+        return api(originalRequest);
+      } catch (err) {
+        window.location.href = "/login";
+        localStorage.removeItem("token");
+        localStorage.removeItem("refreshToken");
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
